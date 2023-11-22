44071644be47b67f3f590b1d666bfd7f9be49ddd
Athosone <werckayrton@gmail.com>
2023-11-22 22:15:18 +0100

    Add search user function

--- src/athosone/git/gitconfig.clj
+++ src/athosone/git/gitconfig.clj
@@ -1,21 +1,20 @@
 (ns athosone.git.gitconfig
-  (:require [clojure.java.shell :as shell]
-            [clojure.java.io :as io]))
+  (:require
+   [clojure.java.io :as io]))
 
 (:import java.io.File)
 
 (def co-authors-file ".git/hooks/prepare-commit-msg")
 
 (defn get-co-authors []
-  (if (.exists (io/file "sha1.cljaaa"))
-    (read-string (slurp "sha1.clj"))
-    (prn "does not exist")))
+  (when (.exists (io/file co-authors-file))
+    (slurp co-authors-file)))
+
 
 (defn replace-co-authors [authors]
-  (spit "sha1.clj" (pr-str authors)))
-
+  (spit co-authors-file (reduce #(str %1 "Co-authored by: " %2 "\n") "" authors)))
 
 (comment
   (get-co-authors)
-
+  (replace-co-authors "toto")
   ())
--- src/athosone/gitlab/pair.clj
+++ src/athosone/gitlab/pair.clj
@@ -1,1 +1,19 @@
-(ns athosone.gitlab.pair)
+(ns athosone.gitlab.pair
+  (:require [clj-http.client :as client]
+            [clojure.edn :as edn]))
+
+(def base-url "https://gitlab.com/api/v4")
+(def search-endpoint "/users?search=")
+
+(def config (edn/read-string (slurp  "./config.edn")))
+(def access-token (:GITLAB_PERSONAL_ACCESS_TOKEN config))
+
+(defn search-user [user]
+  (let [response (client/get (str base-url search-endpoint user)
+                             {:headers {"Private-Token" access-token}
+                              :as :json})]
+    (map #(:username %1) (:body response))))
+
+(comment
+  (search-user "athosone")
+  ())
